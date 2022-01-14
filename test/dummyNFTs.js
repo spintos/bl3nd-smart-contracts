b1c7ae698f4b79720debb7711827cb5f510f41b7
Ilan <ilanolkies@outlook.com>
2022-01-14 23:04:07 +0100

    Add Loot dummy

---
+++ test/dummyNFTs.js
@@ -0,0 +1,29 @@
+const PremintedNFT = artifacts.require('PremintedNFT')
+
+/**
+ * BAYC
+ * https://etherscan.io/address/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d
+ */
+const baycIds = [
+  3650, // https://opensea.io/assets/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/3650
+  4671, // https://opensea.io/assets/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/4671
+  3368 // https://opensea.io/assets/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/3368
+]
+
+const createBayc = () => PremintedNFT.new('BoredApeYachtClub', 'BAYC', baycIds)
+
+/**
+ * Loot
+ * https://etherscan.io/address/0xff9c1b15b16263c61d017ee9f65c50e4ae0113d7
+ */
+const lootIds = [
+  5229,
+  5917,
+  1441
+]
+
+const createLoot = () => PremintedNFT.new('Loot', 'LOOT', lootIds)
+module.exports = {
+  baycIds, createBayc,
+  lootIds, createLoot
+}
--- test/premintedNFT.test.js
+++ test/premintedNFT.test.js
@@ -1,13 +1,15 @@
 const assert = require('assert')
-const PremintedNFT = artifacts.require('PremintedNFT')
+const { createBayc, baycIds, createLoot, lootIds } = require('./dummyNFTs')
 
 contract('PremintedNFT', (accounts) => {
-  it('mints NFTs on creation', async () => {
-    const ids = [3650, 4671, 3368]
-    const premintedNFT = await PremintedNFT.new('BoredApeYachtClub', 'BAYC', ids)
+  for (let nft of [[createBayc, baycIds], [createLoot, lootIds]]) {
+    it('mints NFTs on creation', async () => {
+      const [create, ids] = nft
+      const premintedNFT = await create()
 
-    for (let id of ids) {
-      assert.equal(await premintedNFT.ownerOf(id), accounts[0])
-    }
-  })
+      for (let id of ids) {
+        assert.equal(await premintedNFT.ownerOf(id), accounts[0])
+      }
+    })
+  }
 })
