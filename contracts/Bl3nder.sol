38a346e51bd4dca69799aa5e5ee3e00d71e05a2e
Ilan <ilanolkies@outlook.com>
2022-01-15 00:42:31 +0100

    Bl3nd Deed

--- contracts/Bl3nder.sol
+++ contracts/Bl3nder.sol
@@ -4,15 +4,54 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Bl3nder {
-  address fakeDeedAddress = address(1000);
+contract Bl3ndDeed {
+  Bl3nder blender;
+  address owner;
+  ERC721 public contract0;
+  uint256 public id0;
+  ERC721 public contract1;
+  uint256 public id1;
 
-  function blend(ERC721 contract0, uint256 id0, ERC721 contract1, uint256 id1) public {
-    contract0.transferFrom(msg.sender, fakeDeedAddress, id0);
-    contract1.transferFrom(msg.sender, fakeDeedAddress, id1);
+  constructor(Bl3nder _blender, address _owner, ERC721 _contract0, uint256 _id0, ERC721 _contract1, uint256 _id1) {
+    blender = _blender;
+    owner = _owner;
+    contract0 = _contract0;
+    id0 = _id0;
+    contract1 = _contract1;
+    id1 = _id1;
   }
 
-  function getDeedAddress(address contractAddress0, uint256 id0, address contractAddress1, uint256 id1) public view returns (address) {
-    return fakeDeedAddress;
+  function unblend () public {
+    contract0.transferFrom(address(this), owner, id0);
+    contract1.transferFrom(address(this), owner, id1);
+
+    blender.unblent(contract0, id0, contract1, id1);
+    selfdestruct(payable(tx.origin));
+  }
+}
+
+contract Bl3nder {
+  mapping(bytes32 => Bl3ndDeed) deeds;
+
+  function blendDeedId(ERC721 contract0, uint256 id0, ERC721 contract1, uint256 id1) public pure returns (bytes32) {
+    return keccak256(abi.encodePacked(address(contract0), id0, address(contract1), id1));
+  }
+
+  function getDeedAddress(ERC721 contract0, uint256 id0, ERC721 contract1, uint256 id1) public view returns (address) {
+    return address(deeds[blendDeedId(contract0, id0, contract1, id1)]);
+  }
+
+  function blend(ERC721 contract0, uint256 id0, ERC721 contract1, uint256 id1) public {
+    Bl3ndDeed deed = new Bl3ndDeed(this, msg.sender, contract0, id0, contract1, id1);
+
+    contract0.transferFrom(msg.sender, address(deed), id0);
+    contract1.transferFrom(msg.sender, address(deed), id1);
+
+    deeds[blendDeedId(contract0, id0, contract1, id1)] = deed;
+  }
+
+  function unblent(ERC721 contract0, uint256 id0, ERC721 contract1, uint256 id1) public {
+    require(msg.sender == address(deeds[blendDeedId(contract0, id0, contract1, id1)]), "Invalid sender");
+    deeds[blendDeedId(contract0, id0, contract1, id1)] = Bl3ndDeed(address(0));
   }
 }
--- test/bl3nder.test.js
+++ test/bl3nder.test.js
@@ -1,8 +1,9 @@
 const assert = require('assert')
 const { createBayc, baycIds, createLoot, lootIds } = require('./dummyNFTs')
 const Bl3nder = artifacts.require('Bl3nder')
+const Bl3ndDeed = artifacts.require('Bl3ndDeed')
 
-contract('Bl3nder', () => {
+contract('Bl3nder', (accounts) => {
   it('craetes a Bl3nd Deed that owns the blended nfts', async () => {
     const bayc = await createBayc()
     const loot = await createLoot()
@@ -14,9 +15,45 @@ contract('Bl3nder', () => {
 
     await bl3nder.blend(bayc.address, baycIds[0], loot.address, lootIds[0])
 
-    const bl3ndDeedAddress = await bl3nder.getDeedAddress(bayc.address, baycIds[0], loot.address, lootIds[0])
+    const blendDeedAddress = await bl3nder.getDeedAddress(bayc.address, baycIds[0], loot.address, lootIds[0])
 
-    assert.equal(await bayc.ownerOf(baycIds[0]), bl3ndDeedAddress)
-    assert.equal(await loot.ownerOf(lootIds[0]), bl3ndDeedAddress)
+    assert.equal(await bayc.ownerOf(baycIds[0]), blendDeedAddress)
+    assert.equal(await loot.ownerOf(lootIds[0]), blendDeedAddress)
+  })
+
+  it('unblends by transferring nfts back and deleting deed', async () => {
+    const bayc = await createBayc()
+    const loot = await createLoot()
+
+    const bl3nder = await Bl3nder.new()
+
+    await bayc.approve(bl3nder.address, baycIds[0])
+    await loot.approve(bl3nder.address, lootIds[0])
+
+    await bl3nder.blend(bayc.address, baycIds[0], loot.address, lootIds[0])
+
+    const blendDeedAddress = await bl3nder.getDeedAddress(bayc.address, baycIds[0], loot.address, lootIds[0])
+    const deed = await Bl3ndDeed.at(blendDeedAddress)
+
+    await deed.unblend()
+
+    assert.equal(await bayc.ownerOf(baycIds[0]), accounts[0])
+    assert.equal(await loot.ownerOf(lootIds[0]), accounts[0])
+    assert.equal(await bl3nder.getDeedAddress(bayc.address, baycIds[0], loot.address, lootIds[0]), '0x0000000000000000000000000000000000000000')
+    assert.equal(await web3.eth.getCode(blendDeedAddress), '0x')
+  })
+
+  it('only deed can set unblent', async () => {
+    const bayc = await createBayc()
+    const loot = await createLoot()
+
+    const bl3nder = await Bl3nder.new()
+
+    await bayc.approve(bl3nder.address, baycIds[0])
+    await loot.approve(bl3nder.address, lootIds[0])
+
+    await bl3nder.blend(bayc.address, baycIds[0], loot.address, lootIds[0])
+
+    await assert.rejects(() => bl3nder.unblent(bayc.address, baycIds[0], loot.address, lootIds[0]))
   })
 })
