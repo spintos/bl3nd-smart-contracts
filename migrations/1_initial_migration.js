86cb1e78aff96111a704dc534e782dd90e86ce0e
Ilan <ilanolkies@outlook.com>
2022-01-15 14:51:54 +0100

    Add deploy script

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
-const Migrations = artifacts.require("Migrations");
+const PremintedNFTFactory = artifacts.require('PremintedNFTFactory')
+const Bl3ndCrypt = artifacts.require('Bl3ndCrypt')
+const Bl3nd = artifacts.require('Bl3nd')
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
-};
+  deployer.deploy(PremintedNFTFactory)
+    .then(() => deployer.deploy(Bl3ndCrypt))
+    .then(() => deployer.deploy(Bl3nd, Bl3ndCrypt.address))
+}
